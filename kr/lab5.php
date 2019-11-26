c0f1a0f2b68c397dd62133bd668906c5726ad2aa
krsalas <kb21krist@yahoo.com>
2019-11-26 07:45:59 +0000

    Test

---
+++ kr/lab5.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<meta http-equiv="content-type" content="text/html;charset=iso-8859-1" />
+<title>Lab 5</title>
+</head>
+<body>
+<?php
+
+/*
+$x += <num> adds a value number to the variable $x.
+$x -= <num> subtracts a value to the variable
+$x *= <num>
+$x /= <num>
+
+Value is now 8.
+Add 2. Value is now 10.
+Subtract 4. Value is now 6.
+Multiply by 5. Value is now 30.
+Divide by 3. Value is now 10.
+Increment value by one. Value is now 11.
+Decrement value by one. Value is now 10.
+
+*/
+
+echo $var += 8;
+
+?>
+</body>
+</html>
